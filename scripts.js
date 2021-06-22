e527bea013beee7f5b7552f3e7da352736ed2a1c
Ryan <ryan3738@gmail.com>
2021-06-22 07:25:30 -0700

    initial setup files

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const result = document.querySelector('#result')
+
+const add = function (x, y) {
+  return x + y
+}
+
+const subtract = function (x, y) {
+  return x - y
+}
+
+const multiply = function (x, y) {
+  return x * y
+}
+const divide = function (x, y) {
+  return x / y
+}
+
+const power = function (x, y) {
+  return Math.pow(x, y)
+}
